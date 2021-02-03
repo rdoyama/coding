32e52986efbb2e8d70184558a7ba195cf4c98fcd
Rafael Doyama <rafaeldoyama97@gmail.com>
2021-02-03 13:20:14 -0300

    Reads the contents of a file and calculates the maximum and average, writing these values to the screen and to an output file. The first value of the file is an integer that represents the size of the array and the remaining integers are the values

--- week4.c
+++ week4.c
@@ -1,39 +1,91 @@
-/* Week 4 - Reading integers from file */
+/* Week 4 - Reading integers from file
+
+   For this assignment, we suppose that each row
+   of the file contains only one integer
+   We also assume that there are no errors in
+   the input file and the size of the array is > 0. */
 
 #include<stdio.h>
 #include<stdlib.h>
 
-int* read_file(FILE *ptr)
+// Reads the file that starts at ptr and assign
+// the values to "array"
+void read_file(FILE *ptr, int **array, int size)
 {
-    int i = 0, size;
-    int *array;
-
-    size = *ptr;
-    array = (int*) malloc(size * sizeof(int));
+    int i = 0, x;
     for (i; i < size; i++)
-        array[i] = *(ptr + i + 1);
-    return array;
+    {
+        fscanf(ptr, "%d", (*array + i));
+    }
 }
 
-void print_array(int *array)
+// Prints an array of given size
+void print_array(int array[], int size)
 {
     int i = 0;
 
-    while (*array != '\0')
+    for (i; i < size; i++)
+        printf("%d\t", array[i]);
+    printf("\n\n");
+}
+
+// Computes the average of a array. Size > 0
+double mean(int array[], int size)
+{
+    double average = 0.0;
+    int i = 0;
+
+    for (i; i < size; i++)
+        average += array[i];
+    
+    return average / size;
+}
+
+// Finds the array's maximum. Size > 0
+int max(int array[], int size)
+{
+    int i = 1, mx = array[0];
+    for (i; i < size; i++)
     {
-        printf("%d\t", *(array + i));
-        i++;
+        if (array[i] > mx)
+            mx = array[i];
     }
+    return mx;
 }
 
 int main(void)
 {
-    FILE *ptr;
-    int *array;
+    FILE *input_file, *output_file;
+    int *array, size, maximum;
+    double average;
 
-    ptr = fopen("data.txt", "r");
-    array = read_file(ptr);
-    print_array(array);
+    // File pointer and array size
+    input_file = fopen("data.txt", "r");
+    fscanf(input_file, "%d", &size);
+    printf("Array size: %d\n", size);
+
+    // Memory allocation, reading and printing the original array
+    array = (int*) malloc(size * sizeof(int));
+    read_file(input_file, &array, size);
+    printf("Original array:\n");
+    print_array(array, size);
+
+    // Computes average and maximum
+    average = mean(array, size);
+    printf("Average = %f\n", average);
+    maximum = max(array, size);
+    printf("Max = %d\n", maximum);
+
+    // Prints the average and maximum to the output file
+    // The first line contains the average and the second
+    // stores the maximum
+    output_file = fopen("answer-hw3", "w");
+    fprintf(output_file, "%.3f\n", average);
+    fprintf(output_file, "%d\n", maximum);
+
+    // Close files
+    fclose(input_file);
+    fclose(output_file);
 
     return 0;
 }
